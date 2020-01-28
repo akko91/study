888a3ff7dbeb61bf9d1e09ca9f4b0c4a4a1ea398
91akk <91akk@DESKTOP-EDOUQQD>
2020-01-28 18:03:35 +0900

    간단한 계산기

---
+++ src/simple_calculator/Simple_calculator.java
@@ -0,0 +1,33 @@
+package simple_calculator;
+
+import java.util.Scanner;
+
+public class Simple_calculator {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("'+','-','*''/'중에 하나 입력");
+		String i = sc.next();
+		System.out.print("첫번째 숫자 입력 =>");
+		int a = sc.nextInt();
+		System.out.println(" ");
+		System.out.print("두번째 숫자 입력 =>");
+		int b = sc.nextInt();
+		System.out.println(" ");
+
+		if (i.equals("+")) {
+			System.out.println(a + b);
+		}
+		if (i.equals("-")) {
+			System.out.println(a - b);
+		}
+		if (i.equals("*")) {
+			System.out.println(a * b);
+		}
+		if (i.equals("/")) {
+			System.out.println(a / b);
+		}
+
+	}
+}
