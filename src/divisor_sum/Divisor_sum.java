7ccf19497f15dd2f9af9493c6118d0812d4316c6
91akk <91akk@DESKTOP-EDOUQQD>
2020-02-13 15:07:45 +0900

    정수 n 의 약수의 합

---
+++ src/divisor_sum/Divisor_sum.java
@@ -0,0 +1,31 @@
+package divisor_sum;
+
+//문제 설명
+//정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+//제한 사항
+//n은 0 이상 3000이하인 정수입니다.
+
+public class Divisor_sum {
+
+	 public static int solution(int n) {
+	      int answer = 0;
+	      
+	      for(int i = 1 ; i <= n; i++ ) {
+	    	  if( n%i == 0) {
+	    		  answer += (n/i) + i;
+	    	  }
+	      }
+	      
+	      answer = answer/2;
+	      
+	      System.out.println(answer);
+	      return answer;
+	  }
+	
+	public static void main(String[] args) {
+		solution(5);
+
+	}
+
+}
